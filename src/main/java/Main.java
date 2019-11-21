4368284c0f6df427d4f674ab2893890cba23c3e4
Lidia Kucab <lidiakucab@Lidias-MacBook-Pro.local>
2019-11-21 22:30:18 +0100

    added new Web Elements and functions

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,17 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Main {
+
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "/Users/lidiakucab/Downloads/chromedriver-2");
+        WebDriver webdriver = new ChromeDriver();
+        webdriver.get("https://demoqa.com/keyboard-events-sample-form/");
+
+        KeyboardEventsSampleForm keyboardEvents = new KeyboardEventsSampleForm(webdriver);
+        keyboardEvents.findName(webdriver,"Lidia");
+        keyboardEvents.findCurrentAddress(webdriver,"Krakow");
+        keyboardEvents.findPermanentAddress(webdriver,"Płaszów");
+        keyboardEvents.submitForm(webdriver);
+    }
 }
